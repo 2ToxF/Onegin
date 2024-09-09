9146e8f4db5c1c26bd1c164b2862e1506baa17cc
Антон Головинский <ragplaychan100@gmail.com>
2024-09-09 18:39:55 +0300

    Fix sort and compare funcs

--- main.cpp
+++ main.cpp
@@ -1,10 +1,12 @@
+#include <ctype.h>
 #include <stdio.h>
 
 const int MAX_STRINGS_LEN = 50;
 
 int compare_direct(char* s1, char* s2);
-int partition (char* arr[], int low, int high);
-void quick_sort(char* arr[], int low, int high);
+int compare_reverse(char* s1, char* s2);
+int partition (char* arr[], int low, int high, int (*compare_func)(char*, char*));
+void quick_sort(char* arr[], int low, int high, int (*compare_func)(char*, char*));
 void swap(char** x, char** y);
 
 int main()
@@ -14,21 +16,30 @@ int main()
 
     FILE* fptr = fopen(FILE_NAME, "r");
     char strings[MAX_STRINGS_NUM][MAX_STRINGS_LEN] = {};
-    char* strptrs[MAX_STRINGS_NUM] = {};
+    char* strptrs_direct_sw[MAX_STRINGS_NUM] = {};
+    char* strptrs_reverse_sw[MAX_STRINGS_NUM] = {};
 
     int real_strings_number = 0;
     for (int i = 0; feof(fptr) == 0; i++)
     {
         fgets(strings[i], MAX_STRINGS_LEN, fptr);
-        strptrs[i] = strings[i];
+        strptrs_direct_sw[i] = strings[i];
+        strptrs_reverse_sw[i] = strings[i];
         real_strings_number++;
     }
+    real_strings_number--;
 
     const int START_OF_MAS = 0, END_OF_MAS = real_strings_number - 1;
-    quick_sort(strptrs, START_OF_MAS, END_OF_MAS);
+    quick_sort(strptrs_direct_sw, START_OF_MAS, END_OF_MAS, compare_direct);
+    quick_sort(strptrs_reverse_sw, START_OF_MAS, END_OF_MAS, compare_reverse);
 
     for (int i = 0; i < real_strings_number; i++)
-        printf("%s", strings[i]);
+        printf("%s", strptrs_direct_sw[i]);
+    printf("\n");
+
+    for (int i = 0; i < real_strings_number; i++)
+        printf("%s", strptrs_reverse_sw[i]);
+    printf("\n");
 
     fclose(fptr); fptr = NULL;
     return 0;
@@ -40,12 +51,12 @@ int compare_direct(char* s1, char* s2)
     int s1ind = 0, s2ind = 0;
     while (s1ind < MAX_STRINGS_LEN && s2ind < MAX_STRINGS_LEN)
     {
-        if ((('A' <= s1[s1ind] && s1[s1ind] <= 'Z') || ('a' <= s1[s1ind] && s1[s1ind] <= 'z')) == 0)
+        if (isalpha(s1[s1ind]) == 0)
         {
             s1ind++;
             continue;
         }
-        if ((('A' <= s2[s2ind] && s2[s2ind] <= 'Z') || ('a' <= s2[s2ind] && s2[s2ind] <= 'z')) == 0)
+        if (isalpha(s2[s2ind]) == 0)
         {
             s2ind++;
             continue;
@@ -62,7 +73,34 @@ int compare_direct(char* s1, char* s2)
 }
 
 
-int partition(char* arr[], int low, int high)
+int compare_reverse(char* s1, char* s2)
+{
+    int s1ind = MAX_STRINGS_LEN-1, s2ind = MAX_STRINGS_LEN-1;
+    while (s1ind >= 0 && s2ind >= 0)
+    {
+        if (isalpha(s1[s1ind]) == 0)
+        {
+            s1ind--;
+            continue;
+        }
+        if (isalpha(s2[s2ind]) == 0)
+        {
+            s2ind--;
+            continue;
+        }
+
+        if (s1[s1ind] != s2[s2ind])
+            return s1[s1ind] - s2[s2ind];
+        s1ind--; s2ind--;
+    }
+
+    if (s1ind != s2ind)
+        return s2ind - s1ind;
+    return 0;
+}
+
+
+int partition(char* arr[], int low, int high, int (*compare_func)(char*, char*))
 {
     // Initialize pivot to be the first element
     char* p = arr[low];
@@ -73,43 +111,42 @@ int partition(char* arr[], int low, int high)
     {
         // Find the first element greater than
         // the pivot (from starting)
-        while (compare_direct(arr[i], p) <= 0 && i <= high - 1)
+        while (compare_func(arr[i], p) <= 0 && i <= high - 1)
             i++;
 
         // Find the first element smaller than
         // the pivot (from last)
-        while (compare_direct(arr[j], p) > 0 && j >= low + 1)
+        while (compare_func(arr[j], p) > 0 && j >= low + 1)
             j--;
 
         if (i < j)
             swap(&arr[i], &arr[j]);
     }
-    printf("pizdec");
     swap(&arr[low], &arr[j]);
 
     return j;
 }
 
 
-void quick_sort(char* arr[], int low, int high)
+void quick_sort(char* arr[], int low, int high, int (*compare_func)(char*, char*))
 {
     if (low < high)
     {
         // call partition function to find Partition Index
-        int pi = partition(arr, low, high);
+        int pi = partition(arr, low, high, compare_func);
 
-        // Recursively call suick_sort() for left and right
+        // Recursively call quick_sort() for left and right
         // half based on Partition Index
-        quick_sort(arr, low, pi - 1);
-        quick_sort(arr, pi + 1, high);
+        quick_sort(arr, low, pi - 1, compare_func);
+        quick_sort(arr, pi + 1, high, compare_func);
     }
 }
 
 
 void swap(char** x, char** y)
 {
-    char** tmp = x;
-    x = y;
-    y = tmp;
+    char* tmp = *x;
+    *x = *y;
+    *y = tmp;
     return;
 }
